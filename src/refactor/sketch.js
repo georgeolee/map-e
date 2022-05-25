f0dfc869d9a9bdf1313c1bea5b42f701c20152fc
georgeolee <geoliverlee@gmail.com>
2022-05-24 19:28:14 -0700

    splits vector to color functions into separate file

---
+++ src/refactor/sketch.js
@@ -0,0 +1,13 @@
+//sketch to be run by the p5 instance
+
+export function sketch(p){
+    p.disableFriendlyErrors = true;
+
+    p.setup = function(){
+
+    }
+
+    p.draw = function(){
+        
+    }
+}
---
+++ src/refactor/vectorEncoding.js
@@ -0,0 +1,36 @@
+//functions for color to vector operations and vice-versa
+
+//radians
+/**
+ * 
+ * 
+ * @param {number} angle - in radians
+ * @returns an object representing rgba color. b is always 128 and a is always 255
+ */
+export function colorFromAngle(angle){
+    //vector components
+    const x = Math.cos(angle);
+    const y = Math.sin(angle);
+
+    const r = Math.min(128 + Math.round(128*x), 255);
+    const g = Math.min(128 + Math.round(128*y), 255);
+
+    return {
+        r: r,
+        g: g,
+        b: 128, //z component is 0
+        a: 255, 
+    }
+}
+
+/**
+ * 
+ * @param {number} r - red channel value from 0-255
+ * @param {number} g - green channel value from 0-255
+ * @returns angle in radians
+ */
+export function angleFromColorRG(r, g){
+    const y = g - 128;
+    const x = r - 128;
+    return Math.atan2(y, x);
+}
